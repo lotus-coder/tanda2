1d98009559c8030146d133782ff3d4317b1d8dd7
slts <lotuflas@gmail.coom>
2020-10-22 10:19:55 +0200

    2ok

--- app/src/main/java/com/example/tanda2/actividad04.java
+++ app/src/main/java/com/example/tanda2/actividad04.java
@@ -2,13 +2,38 @@ package com.example.tanda2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class actividad04 extends AppCompatActivity {
+
+    private Button botonsito;
+    private EditText edad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actividad04);
+        botonsito = (Button) findViewById(R.id.Verify);
+        edad = (EditText) findViewById(R.id.Edad);
+
+        botonsito.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(actividad04.this,actividad041.class);
+
+                if(Integer.parseInt(edad.getText().toString())>17){
+                   i.putExtra("Edad",true);
+                }else{
+                    i.putExtra("Edad",false);
+                }
+                startActivity(i);
+            }
+        });
     }
+
+
 }
--- app/src/main/java/com/example/tanda2/actividad041.java
+++ app/src/main/java/com/example/tanda2/actividad041.java
@@ -3,12 +3,23 @@ package com.example.tanda2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class actividad041 extends AppCompatActivity {
+
+    TextView mayor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actividad041);
+        Bundle father =  getIntent().getExtras();
+
+        mayor = (TextView) findViewById(R.id.MayorDeEdad);
+        if(father.getBoolean("Edad")){
+            mayor.setText("Es mayor de edad.");
+        }else{
+            mayor.setText("No es mayor de edad.");
+        }
     }
 }
